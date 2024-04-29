45698ae1f972a3794bf55abc9580b3b40d4a33ec
ilevine3710 <isl225@lehigh.edu>
2024-04-29 16:10:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const path = require("path");
 const app = express();
 
 
-app.use(express.static(
-    path.resolve(__dirname, "public")
-  ));
-  
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.listen(3000, () => console.log("Starting up Word Bounce"));

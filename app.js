d987e55b81f6d491b1cfc70f59b16773861e00f0
ilevine3710 <isl225@lehigh.edu>
2024-04-29 15:50:32 -0400

    font font

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+
+app.use(express.static(
+    path.resolve(__dirname, "public")
+  ));
+  
+app.listen(3000, () => console.log("Starting up Word Bounce"));

674dfeb6d63133f1d984775e95a362f00115eecb
ilevine3710 <isl225@lehigh.edu>
2024-04-30 11:57:11 -0400

    BUTTONS!

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const app = express();
 
 
 app.use(express.static(path.resolve(__dirname, "public")));
+app.get('/button1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
+app.get('/button2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/rounds.html'));
+});
 app.listen(3000, () => console.log("Starting up Word Bounce"));
